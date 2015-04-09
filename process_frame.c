95aeb95b07851a485959982f3c1228230dee2198
oscar <oscar@oscar.(none)>
2015-04-09 17:31:20 +0200

    derivatives in x- and y-direction

--- process_frame.c
+++ process_frame.c
@@ -14,41 +14,40 @@
 
 #define IMG_SIZE NUM_COLORS*(OSC_CAM_MAX_IMAGE_WIDTH/2)*(OSC_CAM_MAX_IMAGE_HEIGHT/2)
 
-const int nc = OSC_CAM_MAX_IMAGE_WIDTH/2;
-const int nr = OSC_CAM_MAX_IMAGE_HEIGHT/2;
-
+const int nc = OSC_CAM_MAX_IMAGE_WIDTH / 2;
+const int nr = OSC_CAM_MAX_IMAGE_HEIGHT / 2;
+void CalcDeriv();
 int TextColor;
 
-
-void ResetProcess()
-{
+void ResetProcess() {
 	//called when "reset" button is pressed
-	if(TextColor == CYAN)
+	if (TextColor == CYAN)
 		TextColor = MAGENTA;
 	else
 		TextColor = CYAN;
 }
 
-
-void ProcessFrame()
-{
+void ProcessFrame() {
 	uint32 t1, t2;
 	char Text[] = "hallo world";
 	//initialize counters
-	if(data.ipc.state.nStepCounter == 1) {
+	//if (data.ipc.state.nStepCounter == 1) {
 		//use for initialization; only done in first step
-		memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
-		TextColor = CYAN;
-	} else {
+		//memset(data.u8TempImage[THRESHOLD], 0, IMG_SIZE);
+		//TextColor = CYAN;
+	//} else {
+
 		//example for time measurement
 		t1 = OscSupCycGet();
+		CalcDeriv();
 		//example for copying sensor image to background image
-		memcpy(data.u8TempImage[BACKGROUND], data.u8TempImage[SENSORIMG], IMG_SIZE);
+		memcpy(data.u8TempImage[BACKGROUND], data.u8TempImage[SENSORIMG],
+		IMG_SIZE);
 		//example for time measurement
 		t2 = OscSupCycGet();
 
 		//example for log output to console
-		OscLog(INFO, "required = %d us\n", OscSupCycToMicroSecs(t2-t1));
+		OscLog(INFO, "required = %d us\n", OscSupCycToMicroSecs(t2 - t1));
 
 		//example for drawing output
 		//draw line
@@ -58,9 +57,25 @@ void ProcessFrame()
 		//draw filled rectangle
 		DrawBoundingBox(80, 100, 110, 120, true, BLUE);
 		DrawString(200, 200, strlen(Text), TINY, TextColor, Text);
-	}
+	//}
 }
 
-
-
+void CalcDeriv() {
+	int c, r;
+	for (r = nc; r < nr * nc - nc; r += nc) {/* we skip the first and last line */
+		for (c = 1; c < nc - 1; c++) {
+			/* do pointer arithmetics with respect to center pixel location */
+			unsigned char* p = &data.u8TempImage[SENSORIMG][r + c];
+			/* implement Sobel f
+			 ilter */
+			int dx = -(int) *(p - nc - 1) + (int) *(p - nc + 1)
+					- 2 * (int) *(p - 1) + 2 * (int) *(p + 1)
+					- (int) *(p + nc - 1) + (int) *(p + nc + 1);
+		int dy = -(int) *(p-nc-1) -2* (int) *(p-nc)- (int) *(p-nc-1)
+				+(int) *(p+nc-1) +2*(int)*(p+nc)+(int) *(p+nc+1);
+		data.u8TempImage[BACKGROUND][r+c] = (uint8) MIN(255, MAX(0,(dx*dx)>>10));
+		data.u8TempImage[THRESHOLD][r+c] = (uint8) MIN(255, MAX(0,128+dy));
+	}
+}
+}
 
